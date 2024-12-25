2462df6330c012a6e6557af0b4e8e8eef75780ad
MariamTurdziladze <mariamt789@gmail.com>
2024-12-25 15:42:34 +0400

    done
    done without local storage (it's in JS task 12)

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 const addTaskBtn = document.getElementById('addTaskBtn')
 const taskList = document.getElementById('taskList');
 const input = document.getElementById('input');
+const totalTaskss = document.getElementById('totalTasks');
+const completedTaskss = document.getElementById('completedTasks');
+
+let totalTasks = 0;
+let completedTasks = 0;
 
 addTaskBtn.addEventListener('click', function() {
     const taskText = input.value;
     if (taskText === "") return;
 
+    totalTasks++;
+    updateCounts();
+
     const newTask = document.createElement('li');
     newTask.innerText = taskText;
 
     const doneBtn = document.createElement('button');
-    doneBtn.innerText = 'DONE' // box with check
+    doneBtn.innerText = 'DONE'
     doneBtn.style.border = "2px solid white";
-    doneBtn.style.backgroundColor = "purple";
+    doneBtn.style.backgroundColor = "blue";
     doneBtn.style.color = "white";
-    doneBtn.style.padding = "5px";
 
 
     doneBtn.addEventListener('click', function () {
         newTask.style.textDecoration = 'line-through';
+        completedTasks++;
+        updateCounts();
     });
 
-    const deleteBtn = document.createElement(`button`);
+    const deleteBtn = document.createElement('button');
     deleteBtn.innerText = 'Delete';
     deleteBtn.style.border = "2px solid white";
-    deleteBtn.style.backgroundColor = "purple";
+    deleteBtn.style.backgroundColor = "blue";
     deleteBtn.style.color = "white";
-    deleteBtn.style.padding = "5px";
 
-    deleteBtn.addEventListener(`click`, function(){
+    deleteBtn.addEventListener('click', function(){
         taskList.removeChild(newTask)
+        totalTasks--;
+        completedTasks--;
+        updateCounts();
     });
 
     newTask.appendChild(doneBtn);
@@ -38,4 +49,9 @@ addTaskBtn.addEventListener('click', function() {
     input.value = '';
 });
 
-localStorage.setItem()
+function updateCounts() {
+    totalTaskss.textContent = `Total Tasks: ${totalTasks}`;
+    completedTaskss.textContent = `Completed tasks: ${completedTasks}`;
+}
+
+updateCounts();
